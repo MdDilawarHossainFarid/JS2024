b59fc745528e1e44dc9271d50a409b0bbf45474b
Md Dilawar Hossain Farid <47987656+MdDilawarHossainFarid@users.noreply.github.com>
2024-10-16 10:59:31 +0530

    Array basic operation

---
+++ array/basic.js
@@ -0,0 +1,29 @@
+// using literal notaion
+
+const fruits = ["Apple","Banana"];
+// console.log(fruits)
+// console.log(fruits.length)
+
+// using Array Construtor
+
+const fruits1 = new Array("Apple","Banana");
+// console.log(fruits1)
+// console.log(fruits1.length);
+
+// Access an array 
+
+// console.log(fruits[0]);
+
+// console.log(fruits[fruits.length - 1]);
+
+// find index of an item
+// console.log(fruits.indexOf("Banana"));
+// console.log(fruits.indexOf("Apple"));
+
+// Check item present or not
+
+// console.log(fruits.includes("Banana"));
+// console.log(fruits.includes("Banana1"));
+
+
+
---
+++ array/tempCodeRunnerFile.js
@@ -0,0 +1,2 @@
+console.log(fruits)
+// console.log(fruits.length)
