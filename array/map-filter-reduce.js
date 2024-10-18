0dc7040bbc2b91a227d6ddde1e92f13a1b8fd4c9
Md Dilawar Hossain Farid <47987656+MdDilawarHossainFarid@users.noreply.github.com>
2024-10-18 20:53:01 +0530

    Array map filter reduce

--- array/map-filter-reduce.js
+++ array/map-filter-reduce.js
@@ -1,4 +1,25 @@
-const array = [1,2,3,4,5];
+// const array1 = [1,2,3,4,5];
 
-const map = array.map((x) => x * 2 );
-console.log(map)
+// const map = array.map((x) => x * 2 );
+// console.log(map)
+
+// const filter1 = array1.filter((x) => x % 2 == 0)
+// // console.log(filter)
+
+// console.log(array1 == filter1)
+
+const friends = [
+  { name: "Anna", books: ["Bible", "Harry Potter"] },
+  { name: "Bob", books: ["War and peace", "Romeo and Juliet"] },
+  { name: "Alice", books: ["The Lord of the Rings", "The Shining"] },
+];
+
+// const output = friends.map((arrayObject) => {
+//     return arrayObject.name + "  " + arrayObject.books;
+// })
+ 
+// const singleValue = friends.reduce((friendsArray,friendsValue)=>{
+//     if(friendsArray[friendsValue.books])
+// },{}) 
+
+// console.log(output)
